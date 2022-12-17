5985f5411fc4753e080f8a9179a0b93b1e321157
elohim18bolts <fduran@fduran.xyz>
2022-12-16 19:02:28 -0500

    Adding "Run Command" to keymap

--- lua/elohim/remap.lua
+++ lua/elohim/remap.lua
@@ -2,4 +2,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader>w", "<C-w>")
+vim.keymap.set("n", "<leader>cc", function() 
+	vim.ui.input( { prompt = "Compile Command: " }, function(input)
+		input = input:gsub(' ', "\\ ")
+		vim.cmd.set(string.format("makeprg=%s",input))
+	end)
+
+end)
 
