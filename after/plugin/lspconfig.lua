c836727981f1d85e0cae8e40a5690129da1e2acb
elohim18bolts <fduran@fduran.xyz>
2023-08-26 16:47:08 -0400

    Quit quickfix buffer after leaving buffer

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -3,15 +3,22 @@ local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.lua_ls.setup {}
+lspconfig.yamlls.setup {}
 lspconfig.rust_analyzer.setup {
 	-- Server-specific settings. See `:help lspconfig-setup`
 	settings = {
 		['rust-analyzer'] = {},
 	},
 }
-vim.api.nvim_create_autocmd('BufWritePre',{
+vim.api.nvim_create_autocmd('BufWritePre', {
 	buffer = buffer,
 	callback = function()
-		vim.lsp.buf.format {async = false }
+		vim.lsp.buf.format { async = false }
 	end
 })
+vim.api.nvim_create_autocmd('BufLeave', {
+
+	buffer = buffer,
+	command = "if &buftype == 'quickfix'|q|endif"
+})
+
