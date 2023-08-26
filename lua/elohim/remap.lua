4c74687ad50a297816a5dd277b51b264d4245698
elohim18bolts <fduran@fduran.xyz>
2023-08-26 16:55:55 -0400

    Add ALEFix keymap

--- lua/elohim/remap.lua
+++ lua/elohim/remap.lua
@@ -3,12 +3,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<leader>w", "<C-w>")
-vim.keymap.set("n", "<leader>cc", function() 
-	vim.ui.input( { prompt = "Compile Command: " }, function(input)
+vim.keymap.set("n", "<leader>cc", function()
+	vim.ui.input({ prompt = "Compile Command: " }, function(input)
 		input = input:gsub(' ', "\\ ")
-		vim.cmd.set(string.format("makeprg=%s",input))
+		vim.cmd.set(string.format("makeprg=%s", input))
 	end)
-
 end)
-
-
+vim.keymap.set('n', '<leader>af', function()
+	vim.cmd('ALEFix\nwrite!')
+	print("ALE fixed document")
+end
+)
