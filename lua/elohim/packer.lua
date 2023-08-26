aae3c4168c23ac5abb9691c8501b4c7c0797f7b3
elohim18bolts <fduran@fduran.xyz>
2023-08-26 16:54:32 -0400

    Add ale and nvim-autopairs plugins

--- lua/elohim/packer.lua
+++ lua/elohim/packer.lua
@@ -4,23 +4,31 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine-moon')
-	  end
-  })
-use("neovim/nvim-lspconfig")
-  use{'nvim-treesitter/nvim-treesitter',run = ':TSUpdate'}
-  use('ThePrimeagen/harpoon')
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine-moon')
+		end
+	})
+	use("neovim/nvim-lspconfig")
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use('ThePrimeagen/harpoon')
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use {
+		'w0rp/ale',
+		ft = { 'python', 'sh', 'rust', 'lua', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket',
+			'vim', 'tex' },
+		cmd = 'ALEEnable',
+		config = 'vim.cmd[[ALEEnable]]'
+	}
+	use("windwp/nvim-autopairs")
 end)
