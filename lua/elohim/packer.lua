0a15b9f27c737238f1f8aa717e93a5f41fc4785e
elohim18bolts <fduran@fduran.xyz>
2023-08-26 16:55:05 -0400

    Add nvim autocompletion plugin

--- lua/elohim/packer.lua
+++ lua/elohim/packer.lua
@@ -31,4 +31,16 @@ return require('packer').startup(function(use)
 		config = 'vim.cmd[[ALEEnable]]'
 	}
 	use("windwp/nvim-autopairs")
+	use {
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+			'hrsh7th/cmp-vsnip',
+			'hrsh7th/vim-vsnip'
+		}
+	}
 end)
