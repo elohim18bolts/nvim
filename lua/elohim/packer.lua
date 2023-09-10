5a6f1be613acbcf316381e6b12226de217a04d5f
elohim18bolts <fduran@fduran.xyz>
2023-09-10 09:02:41 -0400

    Changed color theme to catppuccin machiato

--- lua/elohim/packer.lua
+++ lua/elohim/packer.lua
@@ -11,13 +11,6 @@ return require('packer').startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine-moon')
-		end
-	})
 	use 'freddiehaddad/feline.nvim'
 	use{
 		'catppuccin/nvim', 
