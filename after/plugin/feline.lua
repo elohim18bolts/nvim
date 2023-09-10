213100c0e85c88998a760711bc406fa78ad01940
elohim18bolts <fduran@fduran.xyz>
2023-09-10 09:04:05 -0400

    Added feline configuration

---
+++ after/plugin/feline.lua
@@ -0,0 +1,19 @@
+local clrs = require("catppuccin.palettes").get_palette()
+local ctp_feline = require('catppuccin.groups.integrations.feline')
+local U = require "catppuccin.utils.colors"
+
+
+require("feline").setup({
+    components = ctp_feline.get(),
+})
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        package.loaded["feline"] = nil
+        package.loaded["catppuccin.groups.integrations.feline"] = nil
+        require("feline").setup {
+            components = require("catppuccin.groups.integrations.feline").get(),
+        }
+    end,
+})
+
